ac8289608649cc085b639bdc35be2ae77d951fd0
QuentinDiaferia <q.diaferia@gmail.com>
2015-03-07 12:07:09 +0100

    commentaires extraitDensite

--- extraitDensite.m
+++ extraitDensite.m
@@ -5,9 +5,9 @@
 function [ densites ] = extraitDensite(img, n, m)
 	% On crée un vecteur qui contiendra les densités de chaque zone.
     densites = zeros(n*m, 1);
-    [lignes1, colonnes1] = size(img);
     
     % On crée une seconde image dont les dimensions sont corrigées afin de correspondre au nombre de zones demandées.
+    [lignes1, colonnes1] = size(img);
     pasLignes = ceil(lignes1/n);
     pasColonnes = ceil(colonnes1/m);
     
@@ -22,14 +22,18 @@ function [ densites ] = extraitDensite(img, n, m)
         end
     end
     
-    % Réinitialisation des compteurs pour le calcul de la densité.
+    % Initialisation des compteurs pour le calcul de la densité.
     i = 1;
     j = 1;
-    
     iDen = 1;
+    
     for i=1:pasLignes:pasLignes*n
         for j=1:pasColonnes:pasColonnes*m
+        
+			% On extrait la zone de l'image à traiter.
             img2 = subimage(imgCor, pasColonnes-1, pasLignes-1, j, i);
+         
+			% Calcul de la densité.
             densites(iDen) = sum(sum(img2)) / (pasColonnes*pasLignes*255);
             iDen = iDen+1;
         end
