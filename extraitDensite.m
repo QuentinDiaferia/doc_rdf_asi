6156000768a94ce7563158e7a9a05ac1b04d7cf3
QuentinDiaferia <q.diaferia@gmail.com>
2015-03-07 12:02:03 +0100

    commentaires extraitDensite

--- extraitDensite.m
+++ extraitDensite.m
@@ -1,17 +1,28 @@
+% Fonction permettant l'extraction de la densite de pixel d'une image.
+% - img : image à traiter ;
+% - n et m : nombre de zones par ligne et par colonne.
+
 function [ densites ] = extraitDensite(img, n, m)
+	% On crée un vecteur qui contiendra les densités de chaque zone.
     densites = zeros(n*m, 1);
     [lignes1, colonnes1] = size(img);
+    
+    % On crée une seconde image dont les dimensions sont corrigées afin de correspondre au nombre de zones demandées.
     pasLignes = ceil(lignes1/n);
     pasColonnes = ceil(colonnes1/m);
     
+    % On remplit cette image de blanc.
     imgCor = ones(pasLignes*n, pasColonnes*m);
     imgCor = 255*imgCor;
     
+    % On copie les valeurs de l'image initiale dans la nouvelle.
     for i=1:lignes1
         for j=1:colonnes1
             imgCor(i,j) = img(i, j);
         end
     end
+    
+    % Réinitialisation des compteurs pour le calcul de la densité.
     i = 1;
     j = 1;
     
